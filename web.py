544bb5eb2650b608a63537e3093912da3ecaa7a3
foypapa <mhfarhan153@gmail.com>
2025-11-13 21:12:41 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Read all images inside static/gallery
+    images = os.listdir("static/gallery")
+    images = [img for img in images if img.lower().endswith(("jpg", "jpeg", "png"))]
+    return render_template("index.html", images=images)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
