0e76430e5e9c9be4f8719a6a9a4d97f81c0a00e5
foypapa <mhfarhan153@gmail.com>
2025-11-13 21:49:24 +0530

    Update web.py

--- web.py
+++ web.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # Read all images inside static/gallery
-    images = os.listdir("static/gallery")
+    images = os.listdir("static/")
     images = [img for img in images if img.lower().endswith(("jpg", "jpeg", "png"))]
     return render_template("index.html", images=images)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
